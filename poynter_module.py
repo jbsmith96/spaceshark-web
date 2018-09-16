afaf68632e8d6152356ff366fb1a47cff3733e40
Jordan Smith <jordan.smith96@outlook.com>
2018-09-16 13:12:20 +1000

    First commit for ENGN4221 Space Shark team
    Hard-coded in Canberra lat/long to prevent Google Maps API error. Added ability to print out current pointing coordinates, useful for outreach

--- poynter_module.py
+++ poynter_module.py
@@ -2,12 +2,12 @@
 
 
 def get_daltdaz(obj_name, ipt_lon, ipt_lat, dt=60):
-	
+
 	import astropy.units as u
 	from astropy.time import Time
-	
+
 	if type(dt) is not u.quantity.Quantity: dt *= u.s
-	
+
 	alt_0, az_0 = get_altaz(obj_name, ipt_lon, ipt_lat, t=Time.now())
 	alt_1, az_1 = get_altaz(obj_name, ipt_lon, ipt_lat, t=Time.now() + dt)
 
@@ -18,7 +18,7 @@ def get_daltdaz(obj_name, ipt_lon, ipt_lat, dt=60):
 
 
 def get_altaz(obj_name, ipt_lon, ipt_lat, t=None):
-	
+
 	#for html scrapping
 	#from lxml import html
 	#from bs4 import BeautifulSoup
@@ -34,16 +34,16 @@ def get_altaz(obj_name, ipt_lon, ipt_lat, t=None):
 	from astroquery.simbad import Simbad as simbad
 
 	import ephem
-	
+
 	if t==None: t=Time.now()
-	
+
 	## Set up the observer
 	obs_el = 100 * u.m
 	loc = EarthLocation.from_geodetic(ipt_lon, ipt_lat, obs_el)
 	my_site = Observer(name='My_Site', location=loc)
 
-	obs_lat = my_site.location.latitude
-	obs_lon = my_site.location.longitude
+	obs_lat = my_site.location.lat
+	obs_lon = my_site.location.lon
 
 	#observer for pyephem
 	ephem_site = ephem.Observer()
@@ -64,7 +64,7 @@ def get_altaz(obj_name, ipt_lon, ipt_lat, t=None):
 	obj_name=obj_name.lower()
 
 	if obj_name in ["sun","mercury","venus","moon","mars","jupiter","saturn","uranus","neptune","pluto"]:
-    
+
 		if obj_name == "sun":       my_planet = ephem.Sun()
 		elif obj_name == "mercury": my_planet = ephem.Mercury()
 		elif obj_name == "venus":   my_planet = ephem.Venus()
@@ -75,7 +75,7 @@ def get_altaz(obj_name, ipt_lon, ipt_lat, t=None):
 		elif obj_name == "uranus":  my_planet = ephem.Uranus()
 		elif obj_name == "neptune": my_planet = ephem.Neptune()
 		elif obj_name == "pluto":   my_planet = ephem.Pluto()
-    
+
 		my_planet.compute(ephem_site)
 		az = my_planet.az * 180 / 3.1415926535
 		alt = my_planet.alt * 180 / 3.1415926535
@@ -86,12 +86,12 @@ def get_altaz(obj_name, ipt_lon, ipt_lat, t=None):
 		issdata=page.json()
 		tstamp=issdata['timestamp'];isslat=issdata['iss_position']['latitude'];isslon=issdata['iss_position']['longitude']
 		#there are issues with just this amount of data as you do not know the altitude of the object
-		#here we fix it to 350 km 
-		issheight=350*u.km 
+		#here we fix it to 350 km
+		issheight=350*u.km
 		isslat=Latitude(isslat,unit=u.deg)
 		isslon=Longitude(isslon,unit=u.deg)
 
-		#there are issues however as this data does NOT contain the altitude so lets try scrapping the html 
+		#there are issues however as this data does NOT contain the altitude so lets try scrapping the html
 		#the issue with fullissdata is that it contains information in NASA style units (M50 Cartesian & M50 Keplerian)
 		page=requests.get("http://spaceflight.nasa.gov/realdata/sightings/SSapplications/Post/JavaSSOP/orbit/ISS/SVPOST.html")
 		#fullissdata=html.fromstring(page.text)
@@ -104,18 +104,17 @@ def get_altaz(obj_name, ipt_lon, ipt_lat, t=None):
 		c=SkyCoord(isslon, isslat, issheight)
 		my_target = FixedTarget(name='ISS', coord=c)
 		az = my_site.altaz(t, my_target).az.deg
-		alt = my_site.altaz(t, my_target).alt.deg				
+		alt = my_site.altaz(t, my_target).alt.deg
 	else:
-		try: 
+		try:
 			q = simbad.query_object(obj_name)
 			c = SkyCoord(q["RA"][0], q["DEC"][0], unit=(u.hourangle, u.deg))
 			my_star = FixedTarget(name='my_star', coord=c)
-        
+
 			az = my_site.altaz(t, my_star).az.deg
-			alt = my_site.altaz(t, my_star).alt.deg				
-		except: 
+			alt = my_site.altaz(t, my_star).alt.deg
+		except:
 			print("Couldn't find Object in Database")
 			alt, az = 0, 0
 
 	return alt, az
-
--- spaceshark.py
+++ spaceshark.py
@@ -6,6 +6,8 @@ import sched, time
 from subprocess import Popen
 import random
 import sys
+import atexit
+import signal
 
 s = sched.scheduler(time.time, time.sleep)
 API_KEY = 'AIzaSyCY8AUysBMDz0d20GuIUaMbyJrr6pL-RYQ'
@@ -16,6 +18,8 @@ DELAY = 60.0
 rand = random.random()
 
 app = Flask(__name__)
+
+
 
 class Objects:
     objects = ["Sun","Mercury","Venus","Moon","Mars","Jupiter","Saturn","Uranus","Neptune","Pluto"]
@@ -28,9 +32,14 @@ def hello():
 @app.route("/_point")
 def point():
     try:
-        print "allo."
-        print request.args.get('temp')
+        #print "allo."
+        #print request.args.get('temp')
 
+        print request.args.get('device_id')
+        print request.args.get('access_token')
+        print request.args.get('address')
+        print request.args.get('object_list')
+        print "\n"
         if request.args.get('temp') == 'List':
             object = request.args.get('object_list')
         else:
@@ -43,18 +52,29 @@ def point():
         # Retrieve address from google map
         response_get = requests.get('https://maps.googleapis.com/maps/api/geocode/json',
                         params={'address': address, 'key': API_KEY})
-        location = response_get.json()['results'][0]['geometry']['location']
-
+        #print response_get
+        # location = response_get.json()['results'][0]['geometry']['location']
+        #
+        # lng = location['lng']
+        # lat = location['lat']
+        lat = -35.2809
+        lng = 149.13
         # Convert coordinates (lng, lat) --> (alt, az)
-        alt, az = get_altaz(object, location['lng'], location['lat'])
+        alt, az = get_altaz(object, lng, lat)
+
+        print("\n\n-------------------")
+        print("I'm moving to %f altitude and %f azimuth\n" % (alt,az))
+        print("-------------------\n")
+
         # Send command to clouddy hardware
         system('curl https://api.particle.io/v1/devices/'+str(device_id)+'/point_alt_az ' \
                '-d access_token='+str(access_token)+' ' \
                '-d "args='+str(alt)+','+str(az)+'"')
 
         # Convert coordinates (lng, lat) --> (alt, az)
-        d_alt, d_az = get_daltdaz(object, location['lng'], location['lat'])
-        #  d_alt, d_az = d_alt*1000, d_az*1000
+        d_alt, d_az = get_daltdaz(object, lng,lat)
+
+        #d_alt, d_az = d_alt*10, d_az*10
         #  print(d_alt, d_az)
 
 
@@ -87,15 +107,28 @@ def delta_update(device_id, access_token, d_alt, d_az, delay):
     system('curl https://api.particle.io/v1/devices/'+str(device_id)+'/track_az ' \
                                                                     '-d access_token='+str(access_token)+' ' \
                                                                     '-d "args='+str(d_az)+'"')
-
+    print("\nd_alt = %s\nd_az\n = %s" % (d_alt,d_az))
     time.sleep(float(delay))
     delta_update(device_id, access_token, d_alt, d_az, delay)
 
     #sc.enter(delay, 1, delta_update, (sc, device_id, access_token, d_alt, d_az,))
 
+
+def exit_handler():
+    if len(PROCESSES) > 0:
+        for process in PROCESSES:
+            print("process:", process)
+            try:
+                kill(process, signal.SIGKILL)
+                print "after"
+            except OSError, e:
+                print "error: ", e.errno
+    print 'My application is ending!'
+
+atexit.register(exit_handler)
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         app.run()
     else:
-        print len(sys.argv)
+        #print len(sys.argv)
         delta_update(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
